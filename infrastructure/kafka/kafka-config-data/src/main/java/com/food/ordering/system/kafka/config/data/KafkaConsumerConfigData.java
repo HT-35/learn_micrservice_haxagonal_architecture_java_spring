53107a1084da1ea8cbfb4ff66cb2bf03ba69271b
HT-35 <huyfa352002@gmail.com>
2025-10-16 04:43:47 +0700

    feat(kafka): Implement Kafka consumer configuration and interface

--- infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaConsumerConfigData.java
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaConsumerConfigData.java
@@ -1,7 +1,8 @@
-
-import lombok.Data;
+package com.food.ordering.system.kafka.config.data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
 
 @Data
 @Configuration
